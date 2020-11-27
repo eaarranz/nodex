4de58f190469745ed3c12758e8e64e428aa0eaa8
Eudald <eaarranz@gmail.com>
2020-11-27 16:20:34 +0100

    fix: added cors

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+const cors = require("cors");
 const deck = require("./deck.json");
+
+app.use(
+  cors({
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+);
 
 app.get("/", (req, res) => res.send("Hello from Nuclio!"));
 
